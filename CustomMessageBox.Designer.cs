726d8e577f83d558eb5ec11410e309c0ae8744ee
devming <cmk330@gmail.com>
2016-04-24 18:29:59 +0900

    Custom MessageBox For C#

---
+++ CustomMessageBox.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CustomMessageBox
+{
+    partial class CustomMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.noButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold);
+            this.confirmButton.Location = new System.Drawing.Point(37, 12);
+            this.confirmButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(239, 103);
+            this.confirmButton.TabIndex = 0;
+            this.confirmButton.Text = "확인";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold);
+            this.cancelButton.Location = new System.Drawing.Point(531, 12);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(231, 103);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "취소";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.questionLabel.Location = new System.Drawing.Point(32, 83);
+            this.questionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(0, 24);
+            this.questionLabel.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.noButton);
+            this.panel1.Controls.Add(this.cancelButton);
+            this.panel1.Controls.Add(this.confirmButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 213);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 124);
+            this.panel1.TabIndex = 3;
+            // 
+            // noButton
+            // 
+            this.noButton.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold);
+            this.noButton.Location = new System.Drawing.Point(284, 12);
+            this.noButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(239, 103);
+            this.noButton.TabIndex = 2;
+            this.noButton.Text = "아니요";
+            this.noButton.UseVisualStyleBackColor = true;
+            this.noButton.Click += new System.EventHandler(this.NoButton_Click);
+            // 
+            // CustomMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(791, 337);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.questionLabel);
+            this.Font = new System.Drawing.Font("굴림", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CustomMessageBox";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Title";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button noButton;
+    }
+}
---
+++ CustomMessageBox.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+ * CustomMessageBox
+ * 터치화면에서 메시지박스 버튼 크기 조절을 위해 다시 만든 메시지 박스 다이얼로그
+ */
+
+namespace CustomMessageBox
+{
+    public partial class CustomMessageBox : Form
+    {
+        public CustomMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        public CustomMessageBox(string title, string question, MessageBoxKind field)
+        {
+            InitializeComponent();
+            this.Text = title;
+            this.questionLabel.Text = question;
+            this.mBoxKind = field;
+
+            if (field == MessageBoxKind.Default)
+            {
+                confirmButton.Visible = false;
+                noButton.Visible = false;
+                cancelButton.Text = "확인";
+            }
+            else if (field == MessageBoxKind.YesNo)
+            {
+                confirmButton.Visible = true;
+                noButton.Visible = false;
+                confirmButton.Text = "예";
+                cancelButton.Text = "아니요";
+            }
+            else // field == MessageBoxKind.YesNoCancel
+            {
+                confirmButton.Visible = true;
+                noButton.Visible = true;
+                confirmButton.Text = "확인";
+                cancelButton.Text = "취소";
+            }
+            // TODO: questionLabel에 한글28자까지 쓰면 다음줄로. <<- 이 기능은 보류. 일단 필요없을듯.
+        }
+
+        public enum MessageBoxKind
+        {
+            Default, YesNo, YesNoCancel 
+        }
+
+        public enum DialogResults
+        {
+            No, Yes, Cancel
+        }
+
+        private MessageBoxKind mBoxKind;
+        private static DialogResults response;
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            setResponse(DialogResults.Yes);
+            this.Close();
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            setResponse(DialogResults.No);
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            if (mBoxKind == MessageBoxKind.YesNo)       // YesNo버튼일 때는 No기능 담당. 레이아웃 위치상 noButton을 표시하면 너무 왼쪽으로 쏠리는 느낌이라 cancel버튼으로 no기능 대체함.
+                setResponse(DialogResults.No);
+            else // (mBoxKind == MessageBoxKind.YesNoCancel) || (mBoxKind == MessageBoxKind.Default)    
+                setResponse(DialogResults.Cancel);
+
+            this.Close();
+        }
+
+        public void setResponse(DialogResults res)
+        {
+            response = res;
+        }
+
+        public static DialogResults getResponse()
+        {
+            return response;
+        }
+
+    }
+}
